ced51b3dd626d1f25e37aa04fca99e3ae1cd4f2a
Cristhian Sevilla <crissev17@gmail.com>
2023-05-31 13:35:30 -0600

    Algoritmo de cotización

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { obtenerDiferencia, calcularMarca, calcularPlan, formatearDinero } from "../helpers";
 
 const CotizadorContext = createContext()
 
@@ -13,16 +14,47 @@ const CotizadorProvider = ({ children }) => {
 
     //Subir al state
     const handleChangeDatos = e => {
-    setDatos({
-        //Creamos una copia
-        ...datos,
-        //escribimos lo nuevo
-        [e.target.name] : e.target.value
-    })
+        setDatos({
+            //Creamos una copia
+            ...datos,
+            //escribimos lo nuevo
+            [e.target.name]: e.target.value
+        })
     }
 
     //error
     const [error, setError] = useState("")
+
+    //resultado final
+    const [resultado, setResultado] = useState(0)
+
+    //Cotizar seguro
+    const cotizarSeguro = () => {
+        //Base
+        let resultado = 2000
+
+        //Obtener diferencia de años
+        const diferencia = obtenerDiferencia(datos.year)
+
+        //Restar 3% por cada año
+        resultado -= ((diferencia * 3) * resultado) / 100
+
+        //americano 15%
+        //Europeo 30%
+        //Asiatico 5%
+        resultado *= calcularMarca(datos.marca)
+
+        //Básico 20%
+        //Completo 50%
+        resultado *= calcularPlan(datos.plan)
+
+        //Formatear dinero
+        resultado = formatearDinero(resultado)
+
+        //Subimos al state el resultado
+        setResultado(resultado)
+
+    }
 
     return (
         <CotizadorContext.Provider
@@ -30,7 +62,9 @@ const CotizadorProvider = ({ children }) => {
                 datos,
                 handleChangeDatos,
                 error,
-                setError
+                setError,
+                cotizarSeguro,
+                resultado
             }}
         >
             {children}
@@ -38,7 +72,7 @@ const CotizadorProvider = ({ children }) => {
     )
 }
 
-export{
+export {
     CotizadorProvider
 }
 
